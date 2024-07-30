f1b0e467b90645d361ce5f53dd093136f3a56819
aruncpacp123 <aruncpacp10@gmail.com>
2024-07-30 15:34:03 +0530

    FIles

---
+++ app/src/main/java/com/example/intend/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.intend;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    Button b1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        b1=(Button) findViewById(R.id.button);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(MainActivity.this,secondpage.class);
+                startActivity(i);
+
+
+            }
+        });
+
+    }
+}
